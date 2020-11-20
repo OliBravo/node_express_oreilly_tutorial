d433c43b924e2e2a52869dadbf8fec23beb6003b
jmalecki <j.malecki@tkhtechnology>
2020-11-20 15:01:20 +0100

    create bootcamp controller

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,6 @@
+const Bootcamp = require("../models/Bootcamp");
+const bootcamp = require("../models/Bootcamp");
+
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
@@ -17,8 +20,13 @@ exports.getBootcamp = (req, res, next) => {
 // @desc    Creates a bootcamp
 // @route   POST /api/v1/bootcamp
 // @access  Private
-exports.createBootcamp = (req, res, next) => {
-  res.status(200).json({ success: true, msg: "Create new bootcamp" });
+exports.createBootcamp = async (req, res, next) => {
+  const bootcamp = await Bootcamp.create(req.body);
+
+  res.status(201).json({
+    success: true,
+    data: bootcamp,
+  });
 };
 
 // @desc    Updates bootcamp
