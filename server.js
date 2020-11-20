4f422e00d42d9dae2717c8bb6fb4f20681155fcc
jmalecki <j.malecki@tkhtechnology>
2020-11-20 12:27:33 +0100

    morgan logger middleware

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const logger = require("./middleware/logger");
+const morgan = require("morgan");
 
 //Route files
 const bootcamps = require("./routes/bootcamps");
@@ -10,7 +10,10 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
-app.use(logger);
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 //Mount routers
 app.use("/api/v1/bootcamps", bootcamps);
@@ -19,5 +22,5 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server riuning in ${process.env.NODE_ENV} and port ${PORT}`)
+  console.log(`Server running in ${process.env.NODE_ENV} and port ${PORT}`)
 );
